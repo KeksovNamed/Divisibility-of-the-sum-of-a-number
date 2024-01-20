15f487b966ce77654b461aba38a1779a6e2586d3
KeksovNamed <157127979+KeksovNamed@users.noreply.github.com>
2024-01-20 10:25:32 +0300

    Update slojenie.c

--- slojenie.c
+++ slojenie.c
@@ -1,4 +1,4 @@
-/*Программа вычисляет сумму цифр числа и делимость на другое v1.0 by Keksovname*/
+/*Программа вычисляет сумму цифр числа и делимость на другое v0.6 by Keksovname*/
 #include <stdio.h>
 #include <stdlib.h>
 #include <conio.h>
@@ -6,10 +6,33 @@
 int main()
 {
 	unsigned int sum, divisor;
+	int divisibilitynumbers=0,divisibilityamounts=0;
 	printf("Введите десятичное целое число и делитель\n");
 	scanf("%d%d", &sum, &divisor);
 	printf("Число равно =%d делитель равно =%d\n", sum, divisor);
+	printf("Теперь проверяем делимость чисел+деление сумм чисел/делитель!\n");
 	int result;
+	int length(int n)   //код взят из гугла
+	{
+		int l = 1;
+		for (; n /= 10; ++l);
+		return l;
+	}
+
+	int lengthnumbers = length(sum);
+	printf("Длина числа =%d\n",lengthnumbers);
+    int final = 0;
+    int one = 1;
+    int tmpfinal = 0;
+    int count1=0;
+    int newfinal=0;
+    for (count1; count1 < lengthnumbers; ++count1)
+    {
+        final = sum / one % 10;
+        newfinal = newfinal + final;
+        one = one * 10;
+    }
+    int summa=newfinal;
 	if (sum>divisor)
 	{
 		result =  sum/divisor;
@@ -39,12 +62,87 @@ int main()
 	printf("Деление суммы числа на число равен=%d\n", result); //выводим значение целого результата.
 	if (bigresult > finalresult) //сравнение полного результата с результатом целым
 	{
-		printf("Делитель =%d,число %d не делится без остатка\n", divisor, sum); //если больше то не делится без остатка
+		divisibilityamounts=0;
+		//printf("Делитель =%d,число %d не делится без остатка\n", divisor, sum); //если больше то не делится без остатка
 	}
 
 	if (bigresult == finalresult)
 	{
-		printf("Делитель =%d,число %d делится без остатка\n", divisor, sum);  //если равно тогда делится
-}
-return 0;
+		divisibilityamounts=1;
+		//printf("Делитель =%d,число %d делится без остатка\n", divisor, sum);  //если равно тогда делится
+	}
+	if (summa < divisor)
+	{
+		int resultnew = divisor / summa;
+		double bigsummanew=summa;
+		double bigdivisornew = divisor;
+		double bigresultnew=bigdivisornew / bigsummanew;
+		double newfinalresult = resultnew;
+		printf("Деление суммы чисел полное(double)  равно=%g\n", bigresultnew);
+		printf("Деление суммы чисел  равно=%d\n", resultnew);
+		if (bigresultnew > newfinalresult)
+		{
+			divisibilitynumbers=0;
+		}
+		if (bigresultnew == newfinalresult)
+		{
+			divisibilitynumbers=1;
+		}
+	}
+	if (summa > divisor)
+	{
+		int resultnew = summa / divisor;
+		double bigsummanew=summa;
+		double bigdivisornew = divisor;
+		double bigresultnew=bigsummanew / bigdivisornew;
+		double newfinalresult = resultnew;
+		printf("Деление суммы чисел полное(double)  равно=%g\n", bigresultnew);
+		printf("Деление суммы чисел  равно=%d\n", resultnew);
+		if (bigresultnew > newfinalresult)
+		{
+			divisibilitynumbers=0;
+		}
+		if (bigresultnew == newfinalresult)
+		{
+			divisibilitynumbers=1;
+		}
+
+	}
+	if (summa == divisor)
+	{
+		int resultnew = summa / divisor;
+		double bigsummanew=summa;
+		double bigdivisornew = divisor;
+		double bigresultnew=bigsummanew / bigdivisornew;
+		double newfinalresult = resultnew;
+		printf("Деление суммы чисел полное(double)  равно=%g\n", bigresultnew);
+		printf("Деление суммы чисел  равно=%d\n", resultnew);
+		if (bigresultnew > newfinalresult)
+		{
+			divisibilitynumbers=0;
+		}
+		if (bigresultnew == newfinalresult)
+		{
+			divisibilitynumbers=1;
+		}
+	}
+	if(divisibilityamounts==0&&divisibilitynumbers==1)
+	{
+		printf("Делитель =%d,число %d не делится без остатка", divisor, sum);
+
+	}
+	if(divisibilityamounts==1&&divisibilitynumbers==0)
+	{
+		printf("Делитель =%d,число %d делится без остатка", divisor, sum);
+
+	}
+	if(divisibilityamounts==1&&divisibilitynumbers==1)
+	{
+		printf("Делитель =%d,число %d делится без остатка", divisor, sum);
+	}
+	if(divisibilityamounts==0&&divisibilitynumbers==0)
+	{
+		printf("Делитель =%d,число %d не делится без остатка", divisor, sum);
+	}
+	return 0;
 }
